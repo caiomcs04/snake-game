ab78608ee597e357a5284f8d9477b6e3427c7035
caiomcs04 <caio6c@yahoo.com.br>
2021-05-19 17:33:22 -0300

    6#Commit(game ready)

--- src/js/main.js
+++ src/js/main.js
@@ -67,6 +67,19 @@ function gameStarter() {
   if (snake[0].y > 15 * box && direction === "down") snake[0].y = 0;
   if (snake[0].y < 0 * box && direction === "up") snake[0].y = 16 * box;
 
+  for(i = 1; i < snake.length; i++){
+    if(snake[0].x === snake[i].x && snake[0].y === snake[i].y){
+      clearInterval(game)
+      alert('Game Over')
+      snake = []
+      snake[0] = {
+        x: 8 * box,
+        y: 8 * box
+      }
+      game = setInterval(gameStarter, 100);
+    }
+  }
+
   bgRender();
   snakeRender();
   foodRender();
